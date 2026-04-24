99af65668c8dc7397541416e2c87f734cb3ab8dc
Om-19 <patilom1954@gmail.com>
2026-04-24 10:15:56 +0530

    23 April

---
+++ SpringBoot3/src/main/java/com/jpa/main/service/AppoinmentService.java
@@ -0,0 +1,47 @@
+package com.jpa.main.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.jpa.main.entity.Appoinment;
+import com.jpa.main.entity.Doctor;
+import com.jpa.main.entity.Patient;
+import com.jpa.main.repository.AppoinmentRepository;
+import com.jpa.main.repository.DoctorRepository;
+import com.jpa.main.repository.PatientRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AppoinmentService {
+
+    private AppoinmentRepository appoinmentRepository;
+    private DoctorRepository doctorRepository;
+    private PatientRepository patientRepository;
+
+    /*
+     * No need for cascading as we dont want to create/delete any new Patient/Doctor
+     * Also Appoinment is manyToOne So generally this relationship has no need for
+     * cascading
+     * But cascade will be required in Patient on list(appoinment) so that i spatien
+     * tgets removed
+     * all appoinments hsould be deleted.
+     */
+    @Transactional
+    public Appoinment createAppoinment(Appoinment appoinment, Long doctorId, Long patientId) {
+        Doctor doctor = doctorRepository.findById(doctorId).orElseThrow();
+        Patient patient = patientRepository.findById(patientId).orElseThrow();
+
+        if (appoinment.getId() != null)
+            throw new IllegalArgumentException("Appoinment Should not have Id");
+
+        appoinment.setPatient(patient);
+        appoinment.setDoctor(doctor);
+
+        patient.getAppoinment().add(appoinment); // to maintain consistency
+
+        return appoinmentRepository.save(appoinment);
+
+    }
+}
